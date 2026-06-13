86fb150a400f83b96e8620892086a56cb9d67c40
Alexandre Silva <alexandreviniciusdasilva@gmail.com>
2026-06-13 12:09:38 -0300

    Second Version: Bad One

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,14 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
+		// In this Version, The Code was modified
+		// Changing the Validation of the Dates
+		// And the updateDates to the class Reservation
+		// Giving it a better skin, but, by another way
+		// I should be keep The Validation at the time
+		// Of instantiating the object on the
+		//Program Class, classifying it as a bad code.
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Scanner sc = new Scanner(System.in);
 
@@ -34,16 +42,15 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for upadates must be futures dates.");
-			} else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date.");
-			} else {
-				r.updateDates(checkIn, checkOut);
+			String error = r.updateDates(checkIn, checkOut);
+			
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+			}
+			else {
 				System.out.println("Reservation: " + r);
 			}
-
+			
 		}
 
 		sc.close();
